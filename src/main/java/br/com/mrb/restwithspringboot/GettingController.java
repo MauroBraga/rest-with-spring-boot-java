bb3d4683c4064451f31796412f67e9be9efc959b
mauro <mrb0305@gmail.com>
2024-06-25 21:09:38 -0300

    Primeiro endpoint

---
+++ src/main/java/br/com/mrb/restwithspringboot/GettingController.java
@@ -0,0 +1,22 @@
+package br.com.mrb.restwithspringboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GettingController {
+
+    private static final  String template = "Hello, %s !";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(value = "/greeting")
+    public Greeting greeting(
+            @RequestParam(value = "name", defaultValue = "World")
+            String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template,name));
+    }
+}
