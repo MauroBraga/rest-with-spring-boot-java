686e4b2acc2cdf8916890cb17c92d7877096b4b0
mauro <mrb0305@gmail.com>
2024-06-28 21:04:58 -0300

    Definicao Logica

--- src/main/java/br/com/mrb/restwithspringboot/controller/MathController.java
+++ src/main/java/br/com/mrb/restwithspringboot/controller/MathController.java
@@ -28,10 +28,15 @@ public class MathController {
     }
 
     private Double convertToDouble(String strNumber) {
-        return  null;
+        if(strNumber==null) return 0D;
+        String number = strNumber.replaceAll(",",".");
+        if(isNumeric(number)) return Double.parseDouble(number);
+        return 0D;
     }
 
-    private boolean isNumeric(String numberOne) {
-        return false;
+    private boolean isNumeric(String strNumber) {
+        if(strNumber==null) return false;
+        String number = strNumber.replaceAll(",",".");
+        return number.matches("[-+]?[0-9]*\\\\.?[0-9]+");
     }
 }
